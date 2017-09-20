77802a7c59d9094aa4032a1b15e4415234497d55
WenNingning <1427916171@qq.com>
2017-09-20 10:44:15 +0800

    创建一个框架

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+/**
+ * 创建类名：Test
+ * 创建时间：2017/9/20 10:41
+ *
+ * @author 温宁宁
+ * @version 1.0
+ */
+public class Test {
+    public static void main(String[] ares){
+        System.out.println("test");
+    }
+}
